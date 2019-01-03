c4b042e291bca87ccbe1ecaf2b100b5eab54ae39
rosiehighsmith <rosie.highsmith@gmail.com>
2019-01-03 07:50:59 -0800

    added nodemon

---
+++ .history/app_20190103074744.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+
+app.get('/', (who, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+app.listen(3000, () => {
+    debug(`listening on port ${chalk.green('3000')}`);
+});
